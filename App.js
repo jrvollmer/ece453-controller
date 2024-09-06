5126afc24411b10785d4fe33948bd2959442f73d
James Vollmer <jrvollmer@wisc.edu>
2024-09-06 14:27:33 -0500

    Make this my app

---
+++ App.js
@@ -0,0 +1,13 @@
+// Keep this here!
+import 'react-native-gesture-handler';
+
+import Controller from './src/components/Controller';
+
+
+export default function App() {
+  return (
+    <>
+      <Controller/>
+    </>
+  );
+}
