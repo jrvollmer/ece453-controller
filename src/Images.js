4d954052b74ef604af1927e273c9657715df1c0f
James Vollmer <jrvollmer@wisc.edu>
2024-11-14 12:04:23 -0600

    Multiple items

--- src/Images.js
+++ src/Images.js
@@ -1,1 +1,12 @@
-export const blueShellImg = require("../assets/images/blue-shell.png");
+const blueShellImg = require("../assets/images/blue-shell.png");
+const greenShellImg = require("../assets/images/green-shell.png");
+const greenShell2Img = require("../assets/images/2-green-shells.png");
+const greenShell3Img = require("../assets/images/3-green-shells.png");
+
+export const ItemImages = [
+    null,
+    greenShellImg,
+    greenShell2Img,
+    greenShell3Img,
+    blueShellImg,
+]
--- src/components/controller/Controller.jsx
+++ src/components/controller/Controller.jsx
@@ -5,7 +5,7 @@ import {useEffect, useRef, useState} from "react";
 import BleManager from "react-native-ble-manager";
 
 import {axisPadStyles, containerStyles, padBorderColor, smallKnobSize} from "../../styles/DefaultStyles";
-import {blueShellImg} from "../../Images";
+import {ItemImages} from "../../Images";
 import Joystick from "./Joystick";
 import ActionButton from "./ActionButton";
 import {CharacteristicUUIDs, ServiceUUIDs} from "../../helpers/ble";
@@ -24,27 +24,43 @@ function Controller(props) {
     const joyXRef = useRef(0);
     const joyYRef = useRef(0);
     const [writeFlag, setWriteFlag] = useState(false);
+    const [itemIndex, setItemIndex] = useState(0);
 
     const beginPress = () => {
         console.log("Begin Press")
         // TODO Could probably remove this, unless we want to enable holding items behind the car (i.e. shielding)
     }
 
-    const endPress = async () => {
-        console.log("Use item")
+
+    // TODO Add a notification listener and a handler to get items, as well as one to handle laps
+
+
+    const writeItemBle = async (itemIdx) => {
         // The MCU can poll the "Use Item" characteristic or set up a callback and set it to 0 after changing
         await BleManager.write(
             props.peripheralId,
             ServiceUUIDs.RCController,
             CharacteristicUUIDs.UseItem,
-            [1] // TODO If we have different items, update this accordingly
+            [itemIdx]
         )
             .then(() => {
-                console.log('Use Item write success');
+                console.log('Use Item write success', itemIdx);
             })
             .catch((error) => {
                 console.log('Use Item write error:', error);
             });
+    }
+
+    const endPress = async () => {
+        console.debug("Use item");
+        setItemIndex((currIdx) => {
+            if (currIdx > 0) {
+                writeItemBle(currIdx);
+                console.debug("item would be", currIdx - 1)
+                return currIdx - 1;
+            }
+            return 3; // TODO This should be 0. Just set to 3 for debugging
+        });
     }
 
     const writeValues = async () => {
@@ -121,8 +137,9 @@ function Controller(props) {
                             height: undefined,
                             aspectRatio: 1,
                             resizeMode: "contain",
+                            opacity: (itemIndex === 0 ? 0 : 1)
                         }}
-                        source={blueShellImg} // TODO Update according to actual item held
+                        source={ItemImages[itemIndex]} // TODO Update according to actual item held
                     />
                 </View>
                 <View style={{flexDirection: "column", rowGap: 25, alignItems: "center"}}>
